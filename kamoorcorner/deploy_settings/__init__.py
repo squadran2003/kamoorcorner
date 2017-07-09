748ecaf5a67e1f4a21fa8ca298abef5661b04a15
Andreas Cormack <cormackandy@hotmail.com>
2017-07-09 17:50:05 +0100

    ameds to enviromental variables

--- kamoorcorner/deploy_settings/__init__.py
+++ kamoorcorner/deploy_settings/__init__.py
@@ -12,7 +12,7 @@ ALLOWED_HOSTS=[
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
 EMAIL_HOST_USER = get_env_variable("squadran2003")
-EMAIL_HOST_PASSWORD = get_env_variable("innocent23")
+
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
