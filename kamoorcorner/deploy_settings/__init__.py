189b84db03a259dd9b6b6cd50698613ea204ff65
Andreas Cormack <cormackandy@hotmail.com>
2017-07-09 17:41:11 +0100

    ameds to enviromental variables

--- kamoorcorner/deploy_settings/__init__.py
+++ kamoorcorner/deploy_settings/__init__.py
@@ -1,7 +1,7 @@
 import dj_database_url
 from kamoorcorner.settings import *
 
-DEBUG=True
+DEBUG=False
 TEMPLATE_DEBUG=DEBUG
 
 ALLOWED_HOSTS=[
@@ -11,6 +11,8 @@ ALLOWED_HOSTS=[
 ]
 
 SECRET_KEY = get_env_variable("SECRET_KEY")
+EMAIL_HOST_USER = get_env_variable('squadran2003')
+EMAIL_HOST_PASSWORD = get_env_variable('innocent23')
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
